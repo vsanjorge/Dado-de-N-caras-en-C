44afbfc8a84e031a30ff2dd5cd21eb931e3b2588
Vsanjorge <vsanjorgeperez@gmail.com>
2020-12-03 08:24:27 +0000

    Conseguida la reiteración del bucle cuando el usuario introduce un valor inválido

--- main.c
+++ main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void clean_stdin(void) // source: https://stackoverflow.com/questions/17318886/fflush-is-not-working-in-linux
+{
+    int c;
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+}
+
 int main(void) {
   int dado = 0;
-
   for (;;) {
-    //system("clear");
     printf("Por favor introduce un número de caras para el dado (0 para salir): ");
-    //scanf("%d", &dado);
     if (scanf("%d", &dado) != 1) {
       printf("EL VALOR INTRODUCIDO DEBE SER UN NÚMERO\n");
-      break;
+      clean_stdin(); // IMPORTANTÍSIMO
     } else if (dado == 0) {
       printf("Saliendo del programa...\n");
       break;
